60fbe35bc8d3abcba8ce07b81b88d88b8d87aa2c
Delacrix Morgan <delacrixmorgan@protonmail.com>
2017-09-14 22:03:42 +0800

    Add Back and Setting Icon - CurrencyFragment Toolbar

--- app/src/main/java/com/delacrixmorgan/squark/CurrencyFragment.java
+++ app/src/main/java/com/delacrixmorgan/squark/CurrencyFragment.java
@@ -3,12 +3,14 @@ package com.delacrixmorgan.squark;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import com.delacrixmorgan.squark.adapter.CurrencyAdapter;
 import com.delacrixmorgan.squark.network.InterfaceAPI;
@@ -26,6 +28,7 @@ import retrofit2.Response;
 public class CurrencyFragment extends Fragment {
     private static String TAG = "CurrencyFragment";
 
+    private android.widget.Toolbar mToolbar;
     private RecyclerView mCurrencyRecyclerView;
     private CurrencyAdapter mCurrencyAdapter;
 
@@ -34,6 +37,7 @@ public class CurrencyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_currency, container, false);
 
+        mToolbar = (Toolbar) rootView.findViewById(R.id.fragment_currency_toolbar);
         mCurrencyRecyclerView = (RecyclerView) rootView.findViewById(R.id.fragment_currency_recycler_view);
 
         return rootView;
@@ -42,6 +46,8 @@ public class CurrencyFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        setupToolbar();
 
         Call<RestWrapper> call = SquarkAPI.getClient().create(InterfaceAPI.class).updateRates("USD");
         call.enqueue(new Callback<RestWrapper>() {
@@ -64,6 +70,18 @@ public class CurrencyFragment extends Fragment {
             public void onFailure(Call<RestWrapper> call, Throwable t) {
                 Log.e(TAG, "onFailure (URL) : " + call.request().url());
                 Log.e(TAG, "onFailure (Message) : " + t.toString());
+            }
+        });
+    }
+
+    private void setupToolbar() {
+        getActivity().setActionBar(mToolbar);
+
+        mToolbar.setNavigationIcon(ContextCompat.getDrawable(getActivity(), R.drawable.ic_arrow_back_white_24dp));
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
             }
         });
     }
