7a3ac6ae175914e93a430a3b60fbde5b8db748fd
Delacrix Morgan <delacrixmorgan@protonmail.com>
2018-07-12 23:16:22 +0800

    Able to transition alpha based on position

--- app/src/main/java/com/delacrixmorgan/squark/SquarkEngine.kt
+++ app/src/main/java/com/delacrixmorgan/squark/SquarkEngine.kt
@@ -63,7 +63,14 @@ object SquarkEngine {
 
                     rowList.map {
                         it.translationX = 0F
-                        it.alpha = 1F
+                        it.quantifierTextView.alpha = 1F
+                        it.resultTextView.alpha = 1F
+
+                        it.nextQuantifierTextView.alpha = 0F
+                        it.nextResultTextView.alpha = 0F
+
+                        it.beforeQuantifierTextView.alpha = 0F
+                        it.beforeResultTextView.alpha = 0F
                     }
                 }
 
@@ -77,7 +84,30 @@ object SquarkEngine {
                         val alpha = movingPixels.absoluteValue * alphaRatio
                         rowList.map {
                             it.translationX = movingPixels
-                            it.alpha = if (it.alpha != alpha) 1F - alpha else it.alpha
+
+                            it.quantifierTextView.alpha = Math.round((1F - alpha) * 10F) / 10F
+                            it.resultTextView.alpha = Math.round((1F - alpha) * 10F) / 10F
+
+                            if (movingPixels > 0) {
+                                it.nextQuantifierTextView.alpha = Math.round(alpha * 10F) / 10F
+                                it.nextResultTextView.alpha = Math.round(alpha * 10F) / 10F
+                            } else {
+                                it.beforeQuantifierTextView.alpha = Math.round(alpha * 10F) / 10F
+                                it.beforeResultTextView.alpha = Math.round(alpha * 10F) / 10F
+                            }
+                        }
+                    } else {
+                        rowList.map {
+                            it.quantifierTextView.alpha = 0F
+                            it.resultTextView.alpha = 0F
+
+                            if (movingPixels > 0) {
+                                it.nextQuantifierTextView.alpha = 1F
+                                it.nextResultTextView.alpha = 1F
+                            } else {
+                                it.beforeQuantifierTextView.alpha = 1F
+                                it.beforeResultTextView.alpha = 1F
+                            }
                         }
                     }
                 }
@@ -88,14 +118,38 @@ object SquarkEngine {
         for (index in 0..9) {
             val tableRow = activity.layoutInflater.inflate(R.layout.view_row, tableLayout, false) as TableRow
 
-            val calculateQuantifier = (this.multiplier * (index + 1))
-            val calculateResult = calculateQuantifier * this.conversionRate
+
+            // PRESENT
+            var calculateQuantifier = (this.multiplier * (index + 1))
+            var calculateResult = calculateQuantifier * this.conversionRate
 
             this.bigQuantifier = BigDecimal(calculateQuantifier).setScale(2, BigDecimal.ROUND_HALF_UP)
             this.bigResult = BigDecimal(calculateResult).setScale(2, BigDecimal.ROUND_HALF_UP)
 
             tableRow.quantifierTextView.text = this.decimalFormat.format(this.bigQuantifier).toString()
             tableRow.resultTextView.text = this.decimalFormat.format(this.bigResult).toString()
+
+
+            // BEFORE
+            calculateQuantifier = (this.multiplier / 10 * (index + 1))
+            calculateResult = calculateQuantifier * this.conversionRate
+
+            this.bigQuantifier = BigDecimal(calculateQuantifier).setScale(2, BigDecimal.ROUND_HALF_UP)
+            this.bigResult = BigDecimal(calculateResult).setScale(2, BigDecimal.ROUND_HALF_UP)
+
+            tableRow.beforeQuantifierTextView.text = this.decimalFormat.format(this.bigQuantifier).toString()
+            tableRow.beforeResultTextView.text = this.decimalFormat.format(this.bigResult).toString()
+
+            // NEXT
+
+            calculateQuantifier = (this.multiplier * 10 * (index + 1))
+            calculateResult = calculateQuantifier * this.conversionRate
+
+            this.bigQuantifier = BigDecimal(calculateQuantifier).setScale(2, BigDecimal.ROUND_HALF_UP)
+            this.bigResult = BigDecimal(calculateResult).setScale(2, BigDecimal.ROUND_HALF_UP)
+
+            tableRow.nextQuantifierTextView.text = this.decimalFormat.format(this.bigQuantifier).toString()
+            tableRow.nextResultTextView.text = this.decimalFormat.format(this.bigResult).toString()
 
             // TODO - Restore Click When Ready
 //            tableRow.setOnClickListener {
@@ -109,8 +163,9 @@ object SquarkEngine {
 
     fun updateTable(rowList: ArrayList<TableRow>) {
         rowList.forEachIndexed { index, tableRow ->
-            val calculateQuantifier = (this.multiplier * (index + 1))
-            val calculateResult = calculateQuantifier * this.conversionRate
+            // PRESENT
+            var calculateQuantifier = (this.multiplier * (index + 1))
+            var calculateResult = calculateQuantifier * this.conversionRate
 
             this.bigQuantifier = BigDecimal(calculateQuantifier).setScale(2, BigDecimal.ROUND_HALF_UP)
             this.bigResult = BigDecimal(calculateResult).setScale(2, BigDecimal.ROUND_HALF_UP)
@@ -118,8 +173,28 @@ object SquarkEngine {
             tableRow.quantifierTextView.text = this.decimalFormat.format(this.bigQuantifier).toString()
             tableRow.resultTextView.text = this.decimalFormat.format(this.bigResult).toString()
 
-            tableRow.quantifierTextView.startAnimation(AnimationUtils.loadAnimation(tableRow.context, R.anim.wobble));
-            tableRow.resultTextView.startAnimation(AnimationUtils.loadAnimation(tableRow.context, R.anim.wobble));
+            // BEFORE
+            calculateQuantifier = (this.multiplier / 10 * (index + 1))
+            calculateResult = calculateQuantifier * this.conversionRate
+
+            this.bigQuantifier = BigDecimal(calculateQuantifier).setScale(2, BigDecimal.ROUND_HALF_UP)
+            this.bigResult = BigDecimal(calculateResult).setScale(2, BigDecimal.ROUND_HALF_UP)
+
+            tableRow.beforeQuantifierTextView.text = this.decimalFormat.format(this.bigQuantifier).toString()
+            tableRow.beforeResultTextView.text = this.decimalFormat.format(this.bigResult).toString()
+
+            // NEXT
+            calculateQuantifier = (this.multiplier * 10 * (index + 1))
+            calculateResult = calculateQuantifier * this.conversionRate
+
+            this.bigQuantifier = BigDecimal(calculateQuantifier).setScale(2, BigDecimal.ROUND_HALF_UP)
+            this.bigResult = BigDecimal(calculateResult).setScale(2, BigDecimal.ROUND_HALF_UP)
+
+            tableRow.nextQuantifierTextView.text = this.decimalFormat.format(this.bigQuantifier).toString()
+            tableRow.nextResultTextView.text = this.decimalFormat.format(this.bigResult).toString()
+
+            tableRow.quantifierTextView.startAnimation(AnimationUtils.loadAnimation(tableRow.context, R.anim.wobble))
+            tableRow.resultTextView.startAnimation(AnimationUtils.loadAnimation(tableRow.context, R.anim.wobble))
         }
     }
 
