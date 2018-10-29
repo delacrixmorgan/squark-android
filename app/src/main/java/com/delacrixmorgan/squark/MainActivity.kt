930163c157a1b84d76935dd3cee690fc5f772895
Delacrix Morgan <delacrixmorgan@protonmail.com>
2018-10-29 22:16:05 +0800

    Refactor and Get Country Codes from raw JSON

--- app/src/main/java/com/delacrixmorgan/squark/MainActivity.kt
+++ app/src/main/java/com/delacrixmorgan/squark/MainActivity.kt
@@ -18,6 +18,7 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * MainActivity
@@ -31,50 +32,28 @@ class MainActivity : AppCompatActivity() {
 
     private var disposable: Disposable? = null
     private var countryDatabase: CountryDatabase? = null
-
-    private var countries: List<Country> = ArrayList()
     private var currencies: List<Currency> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseAnalytics.getInstance(this)
-
         setContentView(R.layout.activity_main)
 
         this.countryDatabase = CountryDatabase.getInstance(this)
+        CountryDataController.populateMaps(this)
         fetchCurrencyData()
     }
 
     private fun fetchCurrencyData() {
         AsyncTask.execute {
-            val countryData = this.countryDatabase?.countryDataDao()?.getCountries()
-            if (countryData == null || countryData.isEmpty()) {
-                initCountries(completion = { error ->
-                    if (error != null) {
-                        Snackbar.make(this.mainContainer, getString(R.string.error_api_countries), Snackbar.LENGTH_SHORT).show()
-                    }
+            val countryData = this.countryDatabase?.countryDataDao()?.getCountries() ?: listOf()
 
-                    initCurrencies()
-                })
+            if (countryData.isEmpty()) {
+                initCurrencies()
             } else {
-                startFragment(CurrencyNavigationFragment.newInstance())
-                CountryDataController.updateDataSet(countryData)
+                launchCurrencyNavigationFragment(countryData)
             }
         }
-    }
-
-    private fun initCountries(completion: (error: Throwable?) -> Unit) {
-        this.disposable = SquarkApiService
-                .create(this)
-                .getCountries()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ result ->
-                    this.countries = result.quotes?.map { Country(code = it.key, name = it.value.capitalize(), rate = 0.0) } ?: arrayListOf()
-                    completion.invoke(null)
-                }, { error ->
-                    completion.invoke(error)
-                })
     }
 
     private fun initCurrencies() {
@@ -85,7 +64,7 @@ class MainActivity : AppCompatActivity() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ result ->
                     this.currencies = result.quotes.map { Currency(code = it.key, rate = it.value) }
-                    if (this.countries.isNotEmpty() && this.currencies.isNotEmpty()) {
+                    if (this.currencies.isNotEmpty()) {
                         insertCountries()
                     }
                 }, {
@@ -95,17 +74,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun insertCountries() {
         val database = this.countryDatabase ?: return
-        this.countries.forEachIndexed { index, country ->
-            this.currencies[index].let {
-                country.rate = it.rate
-                AsyncTask.execute {
-                    database.countryDataDao().insertCountry(country)
-                }
+        val countries = arrayListOf<Country>()
+
+        CountryDataController.countryMap.forEach { country ->
+            val currency = this.currencies.firstOrNull {
+                it.code == "USD${country.key}"
+            }
+
+            if (currency != null) {
+                countries.add(Country(code = country.key, name = country.value, rate = currency.rate))
+            }
+        }
+
+        countries.forEach {
+            AsyncTask.execute {
+                database.countryDataDao().insertCountry(it)
             }
         }
 
         PreferenceHelper.getPreference(this)[PreferenceHelper.UPDATED_TIME_STAMP] = Date().time
-        CountryDataController.updateDataSet(this.countries)
+        launchCurrencyNavigationFragment(countries)
+    }
+
+    private fun launchCurrencyNavigationFragment(countries: List<Country>) {
+        CountryDataController.updateDataSet(countries)
         startFragment(CurrencyNavigationFragment.newInstance())
     }
 
