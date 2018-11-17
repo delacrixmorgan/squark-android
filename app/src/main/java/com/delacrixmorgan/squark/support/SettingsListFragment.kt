6100e52c06328a7dfcb80e17ce197751b724935e
Morgan Koh <morgan@apptivitlab.com>
2018-11-17 11:25:20 +0800

    Add countryNavigationActivity to switch between fragments

--- app/src/main/java/com/delacrixmorgan/squark/support/SettingsListFragment.kt
+++ app/src/main/java/com/delacrixmorgan/squark/support/SettingsListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.delacrixmorgan.squark.R
+import kotlinx.android.synthetic.main.fragment_settings_list.*
 
 /**
  * SettingsListFragment
@@ -20,15 +21,35 @@ class SettingsListFragment : Fragment() {
         fun newInstance() = SupportListFragment()
     }
 
+//    private lateinit var dataBinding: FragmentSettingsListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_support_list, container, false)
+//        this.dataBinding = FragmentSettingsListBinding.inflate(inflater, container, false)
+//        return this.dataBinding.root
+        return inflater.inflate(R.layout.fragment_settings_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        this.creditsViewGroup.setOnClickListener {
+
+        }
+
+        this.shareViewGroup.setOnClickListener {
+
+        }
+
+        this.sourceCodeViewGroup.setOnClickListener {
+
+        }
     }
 }
