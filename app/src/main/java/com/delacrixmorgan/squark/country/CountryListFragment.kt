d5b152126facef962c8f477942e39ce3a2f2c3be
Morgan Koh <delacrixmorgan@pm.me>
2018-11-20 23:01:36 +0800

    Refactor CountryList Navigation

--- app/src/main/java/com/delacrixmorgan/squark/country/CountryListFragment.kt
+++ app/src/main/java/com/delacrixmorgan/squark/country/CountryListFragment.kt
@@ -1,9 +1,8 @@
 package com.delacrixmorgan.squark.country
 
-import android.annotation.SuppressLint
 import android.app.Activity
+import android.os.AsyncTask
 import android.os.Bundle
-import android.os.Handler
 import android.text.InputType
 import android.view.*
 import androidx.appcompat.widget.SearchView
@@ -17,7 +16,7 @@ import com.delacrixmorgan.squark.common.PreferenceHelper.get
 import com.delacrixmorgan.squark.common.PreferenceHelper.set
 import com.delacrixmorgan.squark.common.getCompatColor
 import com.delacrixmorgan.squark.common.getFilteredCountries
-import com.delacrixmorgan.squark.data.SquarkWorkerThread
+import com.delacrixmorgan.squark.common.performHapticContextClick
 import com.delacrixmorgan.squark.data.api.SquarkApiService
 import com.delacrixmorgan.squark.data.controller.CountryDataController
 import com.delacrixmorgan.squark.data.controller.CountryDatabase
@@ -28,9 +27,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_country_list.*
-import java.text.SimpleDateFormat
 import java.util.*
-
 
 /**
  * CountryListFragment
@@ -66,21 +63,13 @@ class CountryListFragment : Fragment(), CountryListListener, MenuItem.OnActionEx
     private var searchMenuItem: MenuItem? = null
 
     private lateinit var countryCode: String
-    private lateinit var workerThread: SquarkWorkerThread
-    private lateinit var layoutManager: LinearLayoutManager
-    private lateinit var simpleDateFormat: SimpleDateFormat
     private lateinit var countryAdapter: CountryRecyclerViewAdapter
 
-    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
 
-        this.workerThread = SquarkWorkerThread(SquarkWorkerThread::class.java.simpleName)
-        this.workerThread.start()
-
         this.database = CountryDatabase.getInstance(requireContext())
-        this.simpleDateFormat = SimpleDateFormat("'Last Updated:' dd/MM/yyyy 'at' hh:mm a")
         this.countryCode = this.arguments?.getString(ARG_BELONGS_TO_COUNTRY_CODE) ?: DEFAULT_COUNTRY_CODE
     }
 
@@ -101,10 +90,8 @@ class CountryListFragment : Fragment(), CountryListListener, MenuItem.OnActionEx
         val context = this.context ?: return
 
         this.countryAdapter = CountryRecyclerViewAdapter(listener = this)
-        this.countryAdapter.updateDataSet(CountryDataController.getCountries(), false)
-        this.layoutManager = LinearLayoutManager(this.activity, RecyclerView.VERTICAL, false)
 
-        this.countryRecyclerView.layoutManager = this.layoutManager
+        this.countryRecyclerView.layoutManager = LinearLayoutManager(this.activity, RecyclerView.VERTICAL, false)
         this.countryRecyclerView.adapter = this.countryAdapter
 
         this.swipeRefreshLayout.setColorSchemeColors(
@@ -115,64 +102,57 @@ class CountryListFragment : Fragment(), CountryListListener, MenuItem.OnActionEx
 
     private fun setupListeners() {
         this.swipeRefreshLayout.setOnRefreshListener {
-            Snackbar.make(this.mainContainer, "Updated Currencies", Snackbar.LENGTH_SHORT).show()
-            this.swipeRefreshLayout.isRefreshing = false
+            this.swipeRefreshLayout.performHapticContextClick()
+            checkIsDataUpdated()
         }
     }
 
     private fun checkIsDataUpdated() {
         val timeStamp = PreferenceHelper.getPreference(requireContext())[PreferenceHelper.UPDATED_TIME_STAMP, PreferenceHelper.DEFAULT_UPDATED_TIME_STAMP]
         val currentTimeStamp = Date().time
-//
-//        if (currentTimeStamp - timeStamp > MILLISECONDS_IN_A_WEEK) {
-//            this.updateTextView.text = getString(R.string.fragment_country_list_title_updating)
-//            updateCurrencyRates()
-//        } else {
-//            this.updateTextView.text = getString(R.string.fragment_country_list_title_updated)
-//            Handler().postDelayed({
-//                if (this.isVisible) {
-//                    this.updateImageView.visibility = View.VISIBLE
-//                    this.updateTextView.text = simpleDateFormat.format(Date(currentTimeStamp))
-//                }
-//            }, 3000)
-//        }
+
+        if (currentTimeStamp - timeStamp > MILLISECONDS_IN_A_WEEK) {
+            updateCurrencyRates()
+        } else {
+            Snackbar.make(this.mainContainer, getString(R.string.fragment_country_list_title_everything_already_updated), Snackbar.LENGTH_SHORT).show()
+            this.swipeRefreshLayout.isRefreshing = false
+        }
     }
 
     private fun updateCurrencyRates() {
-        this.workerThread.postTask(Runnable {
+        AsyncTask.execute {
             val countries = this.database?.countryDataDao()?.getCountries() ?: arrayListOf()
+            if (countries.isNotEmpty()) {
+                this.disposable = SquarkApiService
+                        .create(requireActivity())
+                        .getCurrencies()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({ result ->
+                            val currencies = result.quotes.map {
+                                Currency(
+                                        code = it.key,
+                                        rate = it.value
+                                )
+                            }
 
-            Handler().post {
-                if (countries.isNotEmpty()) {
-                    this.disposable = SquarkApiService
-                            .create(requireActivity())
-                            .getCurrencies()
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe({ result ->
-                                val currencies = result.quotes.map {
-                                    Currency(
-                                            code = it.key,
-                                            rate = it.value
-                                    )
-                                }
-
-                                if (currencies.isNotEmpty()) {
-                                    updateCurrencies(
-                                            countries = countries,
-                                            currencies = currencies
-                                    )
-                                }
-                            }, {
-                                Snackbar.make(this.mainContainer, getString(R.string.error_api_countries), Snackbar.LENGTH_SHORT).show()
-                            })
-                }
+                            if (currencies.isNotEmpty()) {
+                                updateCurrencies(
+                                        countries = countries,
+                                        currencies = currencies
+                                )
+                            }
+                            this.swipeRefreshLayout.isRefreshing = false
+                        }, {
+                            Snackbar.make(this.mainContainer, getString(R.string.error_api_countries), Snackbar.LENGTH_SHORT).show()
+                            this.swipeRefreshLayout.isRefreshing = false
+                        })
             }
-        })
+        }
     }
 
     private fun updateCurrencies(countries: List<Country>, currencies: List<Currency>) {
-        this.workerThread.postTask(Runnable {
+        AsyncTask.execute {
             countries.forEach { country ->
                 if (country.code != DEFAULT_COUNTRY_CODE) {
                     val updateCurrency = currencies.find {
@@ -187,31 +167,32 @@ class CountryListFragment : Fragment(), CountryListListener, MenuItem.OnActionEx
             }
 
             CountryDataController.updateDataSet(countries)
-            this@CountryListFragment.activity?.runOnUiThread {
+            this.activity?.runOnUiThread {
                 if (this.isVisible) {
                     val currentTimeStamp = Date().time
-                    PreferenceHelper.getPreference(requireContext())[PreferenceHelper.UPDATED_TIME_STAMP] = currentTimeStamp
 
-//                    this.updateTextView.text = simpleDateFormat.format(Date(currentTimeStamp))
-//                    this.updateImageView.visibility = View.VISIBLE
+                    PreferenceHelper.getPreference(requireContext())[PreferenceHelper.UPDATED_TIME_STAMP] = currentTimeStamp
+                    Snackbar.make(this.mainContainer, getString(R.string.fragment_country_list_title_updated), Snackbar.LENGTH_SHORT).show()
                 }
             }
-        })
+        }
     }
 
     private fun updateDataSet(searchText: String? = null, searchMode: Boolean = false) {
-        if (!this.isVisible) return
         val filterCountries = CountryDataController.getFilteredCountries(searchText) as MutableList<Country>
         val selectedCountry = filterCountries.firstOrNull { it.code == this.countryCode }
 
-        selectedCountry?.let {
-            filterCountries.remove(it)
+        selectedCountry?.let { country ->
+            filterCountries.remove(country)
             filterCountries.sortBy { it.code }
-            filterCountries.add(0, it)
+            filterCountries.add(0, country)
         }
 
-        this.emptyStateViewGroup.visibility = if (filterCountries.isEmpty()) View.VISIBLE else View.GONE
         this.countryAdapter.updateDataSet(filterCountries, searchMode)
+
+        if (this.isVisible) {
+            this.emptyStateViewGroup.visibility = if (filterCountries.isEmpty()) View.VISIBLE else View.GONE
+        }
     }
 
     override fun onCountrySelected(country: Country) {
@@ -236,7 +217,6 @@ class CountryListFragment : Fragment(), CountryListListener, MenuItem.OnActionEx
     }
 
     override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
-//        this.updateViewGroup.visibility = View.GONE
         this.searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 updateDataSet(query, true)
@@ -280,8 +260,6 @@ class CountryListFragment : Fragment(), CountryListListener, MenuItem.OnActionEx
 
     override fun onDestroy() {
         CountryDatabase.destroyInstance()
-        this.workerThread.quit()
-
         super.onDestroy()
     }
 }
