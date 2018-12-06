5b72e3b47363ca17792da9db1f094db1a13fc139
Morgan Koh <delacrixmorgan@pm.me>
2018-12-06 22:23:15 +0800

    Refactor to CurrencyInstrumentedTests

--- data/src/androidTest/java/com/delacrixmorgan/squark/data/CurrencyInstrumentedTests.kt
+++ data/src/androidTest/java/com/delacrixmorgan/squark/data/CurrencyInstrumentedTests.kt
@@ -3,24 +3,26 @@ package com.delacrixmorgan.squark.data
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.delacrixmorgan.squark.data.model.Country
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
+/**
+ * CurrencyInstrumentedTests
+ * squark-android
+ *
+ * Created by Delacrix Morgan on 06/12/2018.
+ * Copyright (c) 2018 licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+ */
+
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        val appContext = InstrumentationRegistry.getInstrumentation().context
-        assertEquals("com.delacrixmorgan.squark.data", appContext.packageName)
-    }
+class CurrencyInstrumentedTests {
+    private val appContext = InstrumentationRegistry.getInstrumentation().context
 
     @Test
     fun populateCountryJson() {
-        val context = InstrumentationRegistry.getInstrumentation().context
         val countries = arrayListOf<Country>()
-        val countryMap = context.getJsonMap(R.raw.data_country, "currencies")
-        val currencyMap = context.getJsonMap(R.raw.data_currency, "quotes")
+        val countryMap = appContext.getJsonMap(R.raw.data_country, "currencies")
+        val currencyMap = appContext.getJsonMap(R.raw.data_currency, "quotes")
 
         org.junit.Assert.assertTrue("Should have at least 1 country", countryMap.isNotEmpty())
         org.junit.Assert.assertTrue("Should have at least 1 currency", currencyMap.isNotEmpty())
