f829855fb85591a3952c48c44f0387ae4513cb40
Morgan Koh <delacrixmorgan@pm.me>
2019-06-13 10:56:37 +0800

    Fix default fallback for Quote Currency

--- app/src/main/java/com/delacrixmorgan/squark/common/SharedPreferenceHelper.kt
+++ app/src/main/java/com/delacrixmorgan/squark/common/SharedPreferenceHelper.kt
@@ -26,7 +26,7 @@ object SharedPreferenceHelper {
             val localeCountry = CountryDataController.getCountries().firstOrNull { it.code == localeCountryCode }
             val fallbackCountryCode = "MYR"
 
-            return if (localeCountryCode != DEFAULT_BASE_CURRENCY_CODE || localeCountry != null) {
+            return if (localeCountryCode != DEFAULT_BASE_CURRENCY_CODE && localeCountry != null) {
                 localeCountryCode
             } else {
                 fallbackCountryCode
