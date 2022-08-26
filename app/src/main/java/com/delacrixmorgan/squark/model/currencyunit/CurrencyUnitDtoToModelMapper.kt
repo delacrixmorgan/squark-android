e871c1a0636221c1c89df702ffc7e74b00ef3384
Morgan Koh <delacrixmorgan@pm.me>
2022-08-27 01:04:01 +0200

    Disable minify and shrink resource

--- app/src/main/java/com/delacrixmorgan/squark/model/currencyunit/CurrencyUnitDtoToModelMapper.kt
+++ app/src/main/java/com/delacrixmorgan/squark/model/currencyunit/CurrencyUnitDtoToModelMapper.kt
@@ -5,7 +5,7 @@ import javax.inject.Inject
 
 class CurrencyUnitDtoToModelMapper @Inject constructor() : Mapper<CurrencyUnitDto, List<CurrencyUnit>> {
     override suspend fun invoke(input: CurrencyUnitDto): List<CurrencyUnit> {
-        return input.currencies.map {
+        return input.currencies.mapNotNull {
             CurrencyUnit(code = it.key, unit = it.value)
         }
     }
